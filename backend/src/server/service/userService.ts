c5444ff3104864e3f52ac9d511293a5217638c90
GabryellReis <gabryellreis32@gmail.com>
2023-12-22 07:37:04 -0300

    adicionando funções de crud no arquivo userService

--- backend/src/server/service/userService.ts
+++ backend/src/server/service/userService.ts
@@ -5,4 +5,41 @@ export class UserService {
     const data = prisma.user.findMany()
     return data;
   }
+  async getUserByRid(rid: string) {
+    const user = prisma.user.findUnique({where: {rid}})
+    return user
+  }
+
+  async registerUser(rid: string, name: string, sector: string, office: string, permissions: string) {
+    const newUser = await prisma.user.create({
+      data: {
+        rid,
+        name,
+        sector,
+        office,
+        permissions
+      }
+    })
+    return newUser;
+  }
+
+  async updateUser(rid: string, name: string, sector: string, office: string, permissions: string) {
+    const updatedUser = await prisma.user.update({
+      where: {rid},
+      data: {
+        rid,
+        name,
+        sector,
+        office,
+        permissions,
+        updatedAt: new Date()
+      }
+    })
+    return updatedUser;
+  }
+
+  async deleteUser(rid: string) {
+    const deletedUser = await prisma.user.delete({where: {rid}})
+    return deletedUser;
+  }
 }
