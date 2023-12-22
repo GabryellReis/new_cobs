50924a0baf1a4ad3dc6dd1bf782ca897a97e0391
GabryellReis <gabryellreis32@gmail.com>
2023-12-22 10:55:33 -0300

    adicionando rota para Home

--- frontend/app/src/routes/Routes.js
+++ frontend/app/src/routes/Routes.js
@@ -1,12 +1,13 @@
-import { View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 const Stack = createNativeStackNavigator()
 export default function Routes() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="login" component={Login} options={{title: 'COBS'}}/>
+      <Stack.Screen name="home" component={Home} options={{title: 'COBS'}}/>
     </Stack.Navigator>
   );
 }
