3fafa783ef75cb90c2d6eda871bef07fc9c6f479
GabryellReis <gabryellreis32@gmail.com>
2023-12-22 08:09:29 -0300

    adicionando rotas de usuário

--- backend/src/server/routes/userRoutes.ts
+++ backend/src/server/routes/userRoutes.ts
@@ -5,5 +5,9 @@ const userRoutes = Router();
 const controller = new UserController();
 
 userRoutes.get("/users", controller.getAllUsers);
+userRoutes.get("/user/:rid", controller.getUserByRid);
+userRoutes.post("/user/register", controller.registerUser);
+userRoutes.put("/user/:rid", controller.registerUser);
+userRoutes.delete("/user/:rid", controller.deleteUser);
 
 export { userRoutes };
