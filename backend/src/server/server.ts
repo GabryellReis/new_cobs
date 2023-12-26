f3fd923bfe2b0c0fca62e4a2ce597416d7faea35
GabryellReis <gabryellreis32@gmail.com>
2023-12-26 11:06:19 -0300

    testando  aplicacao

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -6,7 +6,7 @@ const app = express()
 const port = 3001
 
 const corsOption = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:8081', 'http://127.0.0.1:8081'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
@@ -15,5 +15,6 @@ app.use(cors(corsOption))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(routes)
+
 
 app.listen(port, () => console.log("SERVER RUNNING IN localhost:3001"))
