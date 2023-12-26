b213b989e4c37643031901ac22d7b431f462d425
GabryellReis <gabryellreis32@gmail.com>
2023-12-26 16:20:50 -0300

    removendo dotenv

--- frontend/app/src/api/connection.js
+++ frontend/app/src/api/connection.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-import 'dotenv/config'
 
 const instance = axios.create({
-  baseURL: `http://${process.env.EXPO_PUBLIC_HOST || 'localhost'}:${process.env.EXPO_PUBLIC_PORT || '3001'}`
+  baseURL: `http://localhost:3001`
 })
 
 export default instance;
