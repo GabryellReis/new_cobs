b88e100699d0430444d9d55963f543563615afc1
GabryellReis <gabryellreis32@gmail.com>
2023-12-27 12:26:37 -0300

    adicionando protocolos de segurança http, chaves ssl

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import { routes } from "./routes"
 import cors from 'cors'
+import https from 'https'
+import fs from 'fs'
 
 const app = express()
 const port = 3001
@@ -11,10 +13,16 @@ const corsOption = {
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
 
+
 app.use(cors(corsOption))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(routes)
 
+
+https.createServer({
+  cert: fs.readFileSync('src/server/ssl/server.crt'),
+  key: fs.readFileSync('src/server/ssl/server.key')
+}, app).listen(3333, () => console.log("Rodando no https"))
 
 app.listen(port, () => console.log("SERVER RUNNING IN localhost:3001"))
