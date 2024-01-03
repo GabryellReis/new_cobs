c4ac95c52d19e12464e6360b8042603b6e466085
GabryellReis <gabryellreis32@gmail.com>
2024-01-03 15:56:31 -0300

    refatorando arquivo de rotas do frontend

--- frontend/app/src/routes/Routes.js
+++ frontend/app/src/routes/Routes.js
@@ -9,6 +9,8 @@ import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 
+const redirectProfile = () =>  history.navigate('/profile')
+const redirectChat = () =>  history.navigate('/chats')
 const Stack = createNativeStackNavigator()
 export default function Routes() {
   const history = useNavigation()
@@ -17,19 +19,19 @@ export default function Routes() {
       <Stack.Screen name="login" component={Login} options={{ title: 'COBS' }} />
       <Stack.Screen name="home" component={Home} options={{
         title: 'HOME', headerRight: () => (
-          <TouchableOpacity onPress={history.navigate('/profile')}>
+          <TouchableOpacity onPress={redirectProfile}>
             <FontAwesome name='user-circle-o' />
           </TouchableOpacity>
         ), headerRight: () => (
-          <TouchableOpacity onPress={history.navigate('/chats')}>
+          <TouchableOpacity onPress={redirectChat}>
             <FontAwesome name='paper-plane' />
           </TouchableOpacity>
         ),
       }} />
-      <Stack.Screen name="bags" component={BagConsult} options={{ title: 'BAGS', headerShown: false }} />
-      <Stack.Screen name="bag/register" component={BagRegister} options={{ title: 'BAGS REGISTER', headerShown: false }} />
-      <Stack.Screen name="bag/:id" component={BagDetail} options={{ title: 'BAGS DETAIL', headerShown: false }} />
-      <Stack.Screen name="chats" component={Chat} options={{ title: 'CHATS', headerShown: false }} />
+      <Stack.Screen name="bags" component={BagConsult} options={{ title: 'BAGS' }} />
+      <Stack.Screen name="bag/register" component={BagRegister} options={{ title: 'BAGS REGISTER' }} />
+      <Stack.Screen name="bag/:id" component={BagDetail} options={{ title: 'BAGS DETAIL' }} />
+      <Stack.Screen name="chats" component={Chat} options={{ title: 'CHATS' }} />
       <Stack.Screen name="chat/:id" component={Chat} />
     </Stack.Navigator>
   );
